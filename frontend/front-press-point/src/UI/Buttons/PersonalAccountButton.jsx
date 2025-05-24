bb0536e9a66fb30bbf1c6e17ec105e5aa5a51d28
Par-196 <153231354+Par-196@users.noreply.github.com>
2025-05-24 13:24:06 +0200

    Ready Header
    Added Button and Logo-text

--- frontend/front-press-point/src/UI/Buttons/PersonalAccountButton.jsx
+++ frontend/front-press-point/src/UI/Buttons/PersonalAccountButton.jsx
@@ -1,8 +1,18 @@
-import './PersonalAccountButton.css'
-
+import Button from '@mui/material/Button'
 
 export default function PersonalAccountButton() {
   return (
-    <button>Click me</button>
+    <Button 
+    variant='contained'
+    sx={{
+        backgroundColor: 'white',
+        color: 'black',
+        width: 270,
+        height: 70,
+        fontSize: 20,
+        fontWeight: 700,
+        fontFamily: 'Inter, sans-serif',
+        textTransform: 'none'
+      }}>Особистий кабінет</Button>
   )
 }
