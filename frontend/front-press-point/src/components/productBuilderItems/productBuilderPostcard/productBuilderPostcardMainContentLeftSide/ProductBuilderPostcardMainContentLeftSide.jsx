0d83eb40b85be296eb6226c451f8b4cc0ee59a55
Par-196 <153231354+Par-196@users.noreply.github.com>
2025-05-31 20:54:04 +0200

    Added template for productBuilderPostcardMainContentLeftSide

---
+++ frontend/front-press-point/src/components/productBuilderItems/productBuilderPostcard/productBuilderPostcardMainContentLeftSide/ProductBuilderPostcardMainContentLeftSide.jsx
@@ -0,0 +1,16 @@
+import './ProductBuilderPostcardMainContentLeftSide.css'
+
+export default function ProductBuilderPostcardMainContentLeftSide()
+{
+    return(
+        <div className='product-builder-postcard-main-content-left-side-container'>  
+            <div className='product-builder-postcard-main-content-left-side-container-left-buttons-box'>
+                <p>content</p>
+            </div>
+            <div className='product-builder-postcard-main-content-left-side-container-right-contant-box'>
+                <p>content</p>
+                <p>content</p>
+            </div>
+        </div>
+    )
+}
--- frontend/front-press-point/src/pages/productBuilderPage/ProductBuilderPage.jsx
+++ frontend/front-press-point/src/pages/productBuilderPage/ProductBuilderPage.jsx
@@ -1,11 +1,14 @@
-import ProductBuilderPostcardHeaderBar from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardHeaderBar/ProductBuilderPostcardHeaderBar';
+import ProductBuilderPostcardHeaderBar from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardHeaderBar/ProductBuilderPostcardHeaderBar.jsx';
+import ProductBuilderPostcardMainContentLeftSide from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardMainContentLeftSide/ProductBuilderPostcardMainContentLeftSide.jsx';
 
 export default function ProductBuilderPage() {
 
     return (
         <div className='product-builder-postcard-container'>
             
-            <ProductBuilderPostcardHeaderBar/>
+            <ProductBuilderPostcardHeaderBar />
+
+            <ProductBuilderPostcardMainContentLeftSide />
         </div>
     );
 }
