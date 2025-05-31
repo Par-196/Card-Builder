43e4474bf52105dbfafb1c719e4c07356ca6ae07
Par-196 <153231354+Par-196@users.noreply.github.com>
2025-05-31 21:03:01 +0200

    Added template for ProductBuilderPostcardMainContentRightSide

---
+++ frontend/front-press-point/src/components/productBuilderItems/productBuilderPostcard/productBuilderPostcardMainContentRightSide/ProductBuilderPostcardMainContentRightSide.jsx
@@ -0,0 +1,10 @@
+import './ProductBuilderPostcardMainContentRightSide.css'
+
+export default function ProductBuilderPostcardMainContentRightSide()
+{
+    return(
+        <div className='product-builder-postcard-main-content-right-side'>
+
+        </div>
+    )
+}
--- frontend/front-press-point/src/pages/productBuilderPage/ProductBuilderPage.jsx
+++ frontend/front-press-point/src/pages/productBuilderPage/ProductBuilderPage.jsx
@@ -1,14 +1,20 @@
+import './ProductBuilderPage.css'
 import ProductBuilderPostcardHeaderBar from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardHeaderBar/ProductBuilderPostcardHeaderBar.jsx';
-import ProductBuilderPostcardMainContentLeftSide from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardMainContentLeftSide/ProductBuilderPostcardMainContentLeftSide.jsx';
+import ProductBuilderPostcardMainContentLeftSide from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardMainContentLeftSide/productBuilderPostcardMainContentLeftSide.jsx';
+import ProductBuilderPostcardMainContentRightSide from '../../components/productBuilderItems/productBuilderPostcard/productBuilderPostcardMainContentRightSide/ProductBuilderPostcardMainContentRightSide.jsx';
 
 export default function ProductBuilderPage() {
 
     return (
         <div className='product-builder-postcard-container'>
-            
-            <ProductBuilderPostcardHeaderBar />
+            <div>
+                <ProductBuilderPostcardHeaderBar />
+            </div>
+            <div className='product-builder-postcard-main-content-box'>
+                <ProductBuilderPostcardMainContentLeftSide />
 
-            <ProductBuilderPostcardMainContentLeftSide />
+                <ProductBuilderPostcardMainContentRightSide />
+            </div>
         </div>
     );
 }
